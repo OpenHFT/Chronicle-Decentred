cf07c2003ea6df50533491e7a6c35494c8131cb6
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2018-08-24 13:00:28 +0100

    Code tidy

--- examples/exchange/src/main/java/town/lost/examples/exchange/api/Side.java
+++ examples/exchange/src/main/java/town/lost/examples/exchange/api/Side.java
@@ -3,73 +3,24 @@ package town.lost.examples.exchange.api;
 import net.openhft.chronicle.core.annotation.NotNull;
 import town.lost.examples.exchange.util.CouldBeNaN;
 
-import java.util.function.Consumer;
-
-import static java.lang.Math.*;
-import static town.lost.examples.exchange.util.Validators.notNaN;
+import static java.lang.Math.nextUp;
 
 public enum Side {
     BUY {
         @Override
         public @NotNull
-        PriceCompareResult compare(double newPrice, double referencePrice, double precision) {
-            double delta = notNaN(Math.abs(newPrice - referencePrice));
-            if (delta <= precision) {
-                return PriceCompareResult.SAME;
-            } else if (newPrice > referencePrice) {
-                return PriceCompareResult.BETTER;
-            } else {
-                return PriceCompareResult.WORSE;
-            }
-        }
-
-        @Override
-        public @CouldBeNaN
-        double improveBy(@CouldBeNaN double price, @CouldBeNaN double adjustment) {
-            return price + adjustment;
-        }
-
-        @Override
-        public @CouldBeNaN
-        double worsenBy(@CouldBeNaN double price, @CouldBeNaN double adjustment) {
-            return price - adjustment;
-        }
-
-        @Override
-        public double getBetter(double price1, double price2) {
-            if (price1 >= price2) {
-                return price1;
-            } else if (price2 > price1) {
-                return price2;
-            } else {
-                // one of them must be NaN
-                throw new IllegalArgumentException("NaN value passed to comparision" + price1 + " vs " + price2);
-            }
-
-        }
-
-        @Override
-        public double getWorse(double price1, double price2) {
-            if (price1 <= price2) {
-                return price1;
-            } else if (price2 < price1) {
-                return price2;
-            } else {
-                // one of them must be NaN
-                throw new IllegalArgumentException("NaN value passed to comparision" + price1 + " vs " + price2);
-            }
+        int compare(double newPrice, double referencePrice, double precision) {
+            if (newPrice > referencePrice + precision)
+                return +1;
+            if (referencePrice > newPrice + precision)
+                return -1;
+            return 0;
         }
 
         @Override
         public @CouldBeNaN
         double roundWorse(@CouldBeNaN double value, @CouldBeNaN double tickSize) {
             return tickSize * Math.floor(value / tickSize);
-        }
-
-        @Override
-        public @CouldBeNaN
-        double roundBetter(@CouldBeNaN double value, @CouldBeNaN double tickSize) {
-            return tickSize * Math.ceil(value / tickSize);
         }
 
         @Override
@@ -80,42 +31,14 @@ public enum Side {
     },
     SELL {
         @Override
-        public PriceCompareResult compare(double newPrice, double referencePrice, double precision) {
+        public int compare(double newPrice, double referencePrice, double precision) {
             return BUY.compare(referencePrice, newPrice, precision);
         }
 
         @Override
         public @CouldBeNaN
-        double improveBy(@CouldBeNaN double price, @CouldBeNaN double adjustment) {
-            return BUY.worsenBy(price, adjustment);
-        }
-
-        @Override
-        public @CouldBeNaN
-        double worsenBy(@CouldBeNaN double price, @CouldBeNaN double adjustment) {
-            return BUY.improveBy(price, adjustment);
-        }
-
-        @Override
-        public double getBetter(double price1, double price2) {
-            return BUY.getWorse(price1, price2);
-        }
-
-        @Override
-        public double getWorse(double price1, double price2) {
-            return BUY.getBetter(price1, price2);
-        }
-
-        @Override
-        public @CouldBeNaN
         double roundWorse(@CouldBeNaN double value, @CouldBeNaN double tickSize) {
-            return BUY.roundBetter(value, tickSize);
-        }
-
-        @Override
-        public @CouldBeNaN
-        double roundBetter(@CouldBeNaN double value, @CouldBeNaN double tickSize) {
-            return BUY.roundWorse(value, tickSize);
+            return tickSize * Math.ceil(value / tickSize);
         }
 
         @Override
@@ -126,23 +49,9 @@ public enum Side {
     };
 
     static final double DEFAULT_PRECISION_FACTOR = 1E-7;
-    private static final Side SIDES[] = {BUY, SELL};
 
     static {
         assert (BUY.ordinal() == 0) && (SELL.ordinal() == 1);
-    }
-
-    public static Side fromId(int ordinal) {
-        return SIDES[ordinal];
-    }
-
-    public static void forEach(Consumer<Side> consumer) {
-        consumer.accept(BUY);
-        consumer.accept(SELL);
-    }
-
-    public static int ticksBetween(double bestPrice, double worstPrice, double tickSize) {
-        return (int) round(abs(bestPrice - worstPrice) / tickSize);
     }
 
     public static double getDefaultPrecision(double tickSize) {
@@ -150,45 +59,13 @@ public enum Side {
     }
 
     @NotNull
-    protected abstract PriceCompareResult compare(double newPrice, double referencePrice, double precision);
+    protected abstract int compare(double newPrice, double referencePrice, double precision);
 
     public boolean isBetterOrSame(double price, double referencePrice, double precision) {
-        return compare(price, referencePrice, precision) != PriceCompareResult.WORSE;
+        return compare(price, referencePrice, precision) >= 0;
     }
-
-    public boolean isBetter(double price, double referencePrice, double precision) {
-        return compare(price, referencePrice, precision) == PriceCompareResult.BETTER;
-    }
-
-    public boolean isWorse(double price, double referencePrice, double precision) {
-        return compare(price, referencePrice, precision) == PriceCompareResult.WORSE;
-    }
-
-    public boolean isWorseOrSame(double price, double referencePrice, double precision) {
-        return compare(price, referencePrice, precision) != PriceCompareResult.BETTER;
-    }
-
-    public boolean isSame(double price, double referencePrice, double precision) {
-        return compare(price, referencePrice, precision) == PriceCompareResult.SAME;
-    }
-
-    public abstract double getWorse(double price1, double price2);
-
-    public abstract double getBetter(double price1, double price2);
-
-    public abstract double improveBy(double price, double adjustment);
-
-    public abstract double worsenBy(double price, double adjustment);
 
     public abstract double roundWorse(double value, double precision);
 
-    public abstract double roundBetter(double value, double precision);
-
     public abstract Side other();
-
-    public enum PriceCompareResult {
-        WORSE, SAME, BETTER
-    }
-
-
 }
