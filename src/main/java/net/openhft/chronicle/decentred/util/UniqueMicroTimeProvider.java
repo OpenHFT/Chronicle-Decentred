46de72c6c6d989071caad8d780b15f091c6b796d
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2018-09-04 16:00:35 +0100

    Use the more accurate timer.

--- src/main/java/net/openhft/chronicle/decentred/util/UniqueMicroTimeProvider.java
+++ src/main/java/net/openhft/chronicle/decentred/util/UniqueMicroTimeProvider.java
@@ -1,22 +1,24 @@
 package net.openhft.chronicle.decentred.util;
 
+import net.openhft.chronicle.core.time.SystemTimeProvider;
 import net.openhft.chronicle.core.time.TimeProvider;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 public enum UniqueMicroTimeProvider implements TimeProvider {
     INSTANCE;
+    private final SystemTimeProvider BASE = SystemTimeProvider.INSTANCE;
     private final AtomicLong lastTime = new AtomicLong();
 
     @Override
     public long currentTimeMillis() {
-        return System.currentTimeMillis();
+        return BASE.currentTimeMillis();
     }
 
     @Override
     public long currentTimeMicros() {
         // not very accurate but light weight in Java.
-        long now = currentTimeMillis() * 1000L;
+        long now = BASE.currentTimeMicros();
         while (true) {
             long value0 = lastTime.get();
             final long value;
