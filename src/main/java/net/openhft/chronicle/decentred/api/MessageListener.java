34251545314978a1a45cbf4078e3158048fe44c7
Per-Åke Minborg <minborg@speedment.com>
2019-02-03 13:04:35 +0100

    Add javadoc to MessageListener

--- src/main/java/net/openhft/chronicle/decentred/api/MessageListener.java
+++ src/main/java/net/openhft/chronicle/decentred/api/MessageListener.java
@@ -3,5 +3,11 @@ package net.openhft.chronicle.decentred.api;
 import net.openhft.chronicle.decentred.dto.SignedMessage;
 
 public interface MessageListener {
+
+    /**
+     * Called by the framework on a signed message
+     *
+     * @param message connected
+     */
     void onMessage(SignedMessage message);
 }
