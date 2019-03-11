0599a1d921c66d6b0a662dffd9e2f28287f4e58b
Dan Lawesson <dan@kodgruvan.se>
2019-03-11 10:22:12 +0100

    Add a give operation to Traffic

--- examples/appreciation/src/main/java/town/lost/examples/appreciation/benchmark/Traffic.java
+++ examples/appreciation/src/main/java/town/lost/examples/appreciation/benchmark/Traffic.java
@@ -9,12 +9,16 @@ import net.openhft.chronicle.decentred.remote.rpc.RPCClient;
 import net.openhft.chronicle.decentred.server.RPCBuilder;
 import net.openhft.chronicle.decentred.util.DecentredUtil;
 import net.openhft.chronicle.salt.Ed25519;
+import org.jetbrains.annotations.NotNull;
 import town.lost.examples.appreciation.api.AppreciationMessages;
 import town.lost.examples.appreciation.api.AppreciationResponses;
+import town.lost.examples.appreciation.dto.Give;
 import town.lost.examples.appreciation.dto.OpeningBalance;
 
 import java.net.InetSocketAddress;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Traffic  {
     private static final double START_AMOUNT = 2_000_000d;
@@ -25,49 +29,95 @@ public class Traffic  {
 
     public Traffic() {}
 
+    private static class Client {
+        private final long accountSeed;
+        private final long address;
+        private final RPCClient<AppreciationMessages, AppreciationResponses> client;
+
+        private Client(long accountSeed, long address, RPCClient<AppreciationMessages, AppreciationResponses> client) {
+            this.accountSeed = accountSeed;
+            this.address = address;
+            this.client = client;
+        }
+
+        public void close() {
+            client.close();
+        }
+
+        @NotNull
+        public String toString() {
+            return "Client(" + name() + ")";
+        }
+
+        @NotNull
+        private String name() {
+            return DecentredUtil.toAddressString(address);
+        }
+
+        private AppreciationMessages toDefault() {
+            return client.toDefault();
+        }
+
+    }
+
     public static void main(String[] args) {
         final String[] addrPair = args[0].split(":");
         final InetSocketAddress socketAddress = InetSocketAddress.createUnresolved(addrPair[0], Integer.parseInt(addrPair[1]));
         System.out.println("Connecting to Gateway at " + socketAddress);
 
-        Stream.of(GIVER/*ACCOUNTS*/).forEachOrdered(accountSeed -> {
-            System.out.println("Setting up keys");
-
+        List<Client> clients = Arrays.stream(ACCOUNTS).mapToObj(accountSeed -> {
             final BytesStore privateKey = DecentredUtil.testPrivateKey(accountSeed);
             final Bytes publicKey = Bytes.allocateDirect(Ed25519.PUBLIC_KEY_LENGTH);
             final Bytes secretKey = Bytes.allocateDirect(Ed25519.SECRET_KEY_LENGTH);
             Ed25519.privateToPublicAndSecret(publicKey, secretKey, privateKey);
 
             long address = DecentredUtil.toAddress(publicKey); // Isn't this the address to use?
-            System.out.println("Account " + accountSeed + " is " + DecentredUtil.toAddressString(address));
+            String name = DecentredUtil.toAddressString(address);
+            System.out.println("Account " + accountSeed + " is " + name);
 
             System.out.println("Setting RPC client");
-            RPCClient<AppreciationMessages, AppreciationResponses> client =
-                RPCBuilder.of(17, AppreciationMessages.class, AppreciationResponses.class)
-                    .secretKey(secretKey)
-                    .publicKey(publicKey)
-                    .createClient(DecentredUtil.toAddressString(address), socketAddress, new Peer.ResponseSink());
+            RPCClient<AppreciationMessages, AppreciationResponses> rpcClient = RPCBuilder.of(17, AppreciationMessages.class, AppreciationResponses.class)
+                .secretKey(secretKey)
+                .publicKey(publicKey)
+                .createClient(name, socketAddress, new Peer.ResponseSink());
+            Client client = new Client(accountSeed, address, rpcClient);
 
-            System.out.println("Waiting for ability to send first message...");
+            System.out.println("Waiting for ability to send second message...");
             Jvm.pause(7000);
+
             System.out.println("Sending CreateAddressRequest");
 
-            client.write(new CreateAddressRequest()
+            client.toDefault().createAddressRequest(new CreateAddressRequest()
                 .address(address)
                 .timestampUS(UniqueMicroTimeProvider.INSTANCE.currentTimeMicros())
                 .publicKey(publicKey)
             );
+            return client;
+        }).collect(Collectors.toList());
+
+        clients.forEach(client -> {
             System.out.println("Waiting for ability to send second message...");
             Jvm.pause(7000);
 
-            System.out.println("Setting account " + accountSeed + " to " + START_AMOUNT);
+            System.out.println("Setting account " + client + " to " + START_AMOUNT);
 
             final OpeningBalance ob = new OpeningBalance()
-                .address(address)
+                .address(client.address)
                 .timestampUS(UniqueMicroTimeProvider.INSTANCE.currentTimeMicros())
-                .init(address, START_AMOUNT);
-            client.write(ob);  // when signing this, the address is garbled!
+                .init(client.address, START_AMOUNT);
+            client.toDefault().openingBalance(ob);
+        });
 
+        System.out.println("Waiting for ability to send second message...");
+        Jvm.pause(7000);
+
+        final Give give = new Give()
+            .address(clients.get(0).address)
+            .timestampUS(UniqueMicroTimeProvider.INSTANCE.currentTimeMicros())
+            .init(clients.get(1).address, 17);
+        clients.get(0).toDefault().give(give);
+
+        clients.forEach(client -> {
             System.out.println("Waiting");
             Jvm.pause(2000);
             System.out.println("Closing");
@@ -75,26 +125,6 @@ public class Traffic  {
 
         });
 
-        /*
-
-        Thread.sleep(10000);
-
-
-        RPCClient<AppreciationMessages, AppreciationRequests> client = getRpcBuilder()
-            .createAccountClient(GIVER, secretKeys.get(GIVER), socketAddress, new ResponseSink());
-
-                client.write(new Give()
-            .timestampUS(UniqueMicroTimeProvider.INSTANCE.currentTimeMicros())
-            .address(DecentredUtil.parseAddress(GIVER))
-            .init(DecentredUtil.parseAddress(TAKER), 10));
-
-        client.close();*/
-
-/*
-        while (System.in.available() == 0) {
-            Thread.sleep(5000);
-        }
-        System.exit(0);*/
 
     }
 
