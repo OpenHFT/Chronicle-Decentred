5b4e5f8a8b7893ce795568794117e083e7a3f44e
Per-Åke Minborg <minborg@speedment.com>
2019-05-13 09:10:55 +0200

    appreciation: Add real benchmark in Traffic

--- examples/appreciation/src/main/java/town/lost/examples/appreciation/benchmark/Traffic.java
+++ examples/appreciation/src/main/java/town/lost/examples/appreciation/benchmark/Traffic.java
@@ -1,6 +1,5 @@
 package town.lost.examples.appreciation.benchmark;
 
-import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.bytes.BytesStore;
 import net.openhft.chronicle.core.Jvm;
 import net.openhft.chronicle.core.time.UniqueMicroTimeProvider;
@@ -9,7 +8,6 @@ import net.openhft.chronicle.decentred.remote.rpc.RPCClient;
 import net.openhft.chronicle.decentred.server.RPCBuilder;
 import net.openhft.chronicle.decentred.util.DecentredUtil;
 import net.openhft.chronicle.decentred.util.KeyPair;
-import net.openhft.chronicle.salt.Ed25519;
 import org.jetbrains.annotations.NotNull;
 import town.lost.examples.appreciation.api.AppreciationMessages;
 import town.lost.examples.appreciation.api.AppreciationResponses;
@@ -19,18 +17,24 @@ import town.lost.examples.appreciation.dto.OpeningBalance;
 import java.net.InetSocketAddress;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class Traffic  {
+import static java.util.stream.Collectors.toList;
+
+public final class Traffic  {
     private static final double START_AMOUNT = 2_000_000d;
 
-    public static final long GIVER = 1;
-    public static final long TAKER = 2;
+    private static final long GIVER = 1;
+    private static final long TAKER = 2;
     private static final long[] ACCOUNTS = {GIVER, TAKER};
 
     public Traffic() {}
 
-    private static class Client {
+    private enum BencmarkState {
+        WARMUP, RUN;
+    }
+
+    private final static class Client {
         private final long accountSeed;
         private final long address;
         private final RPCClient<AppreciationMessages, AppreciationResponses> client;
@@ -67,21 +71,22 @@ public class Traffic  {
         System.out.println("Connecting to Gateway at " + socketAddress);
 
         List<Client> clients = Arrays.stream(ACCOUNTS).mapToObj(accountSeed -> {
-            KeyPair kp = new KeyPair(accountSeed);
+            final KeyPair kp = new KeyPair(accountSeed);
             final BytesStore publicKey = kp.publicKey;
 
-            long address = DecentredUtil.toAddress(publicKey); // Isn't this the address to use?
-            String name = DecentredUtil.toAddressString(address);
+            final long address = DecentredUtil.toAddress(publicKey); // Isn't this the address to use?
+            final String name = DecentredUtil.toAddressString(address);
             System.out.println("Account " + accountSeed + " is " + name);
 
             System.out.println("Setting RPC client");
-            RPCClient<AppreciationMessages, AppreciationResponses> rpcClient = RPCBuilder.of(17, AppreciationMessages.class, AppreciationResponses.class)
+            final RPCClient<AppreciationMessages, AppreciationResponses> rpcClient = RPCBuilder.of(17, AppreciationMessages.class, AppreciationResponses.class)
                 .keyPair(kp)
                 .createClient(name, socketAddress, new Peer.ResponseSink());
-            Client client = new Client(accountSeed, address, rpcClient);
 
-            System.out.println("Waiting some time before sending first client message to " + name);
-            Jvm.pause(7000);
+            final Client client = new Client(accountSeed, address, rpcClient);
+
+/*            System.out.println("Waiting some time before sending first client message to " + name);
+            Jvm.pause(1000);*/
 
             System.out.println("Sending CreateAddressRequest");
 
@@ -91,11 +96,11 @@ public class Traffic  {
                 .publicKey(publicKey)
             );
             return client;
-        }).collect(Collectors.toList());
+        }).collect(toList());
 
         clients.forEach(client -> {
             System.out.println("Waiting some time before sending second client message to " + client.name());
-            Jvm.pause(7000);
+            Jvm.pause(1000);
 
             System.out.println("Setting account " + client + " to " + START_AMOUNT);
 
@@ -107,24 +112,64 @@ public class Traffic  {
         });
 
         System.out.println("Waiting some time before sending give message...");
-        Jvm.pause(15_000);
+        Jvm.pause(1_000);
 
         final Give give = new Give()
             .address(clients.get(0).address)
             .timestampUS(UniqueMicroTimeProvider.INSTANCE.currentTimeMicros())
-            .init(clients.get(1).address, 17);
+            .init(clients.get(1).address, 1);
         clients.get(0).toDefault().give(give);
 
+
+        System.out.println("Waiting some time before sending a second give message...");
+        Jvm.pause(1_000);
+        final Give give2 = new Give()
+            .address(clients.get(0).address)
+            .timestampUS(UniqueMicroTimeProvider.INSTANCE.currentTimeMicros())
+            .init(clients.get(1).address, 1);
+        clients.get(0).toDefault().give(give2);
+
+
+
         System.out.println("Done.");
+        Jvm.pause(2000);
+
+        for (BencmarkState state : BencmarkState.values()) {
+
+            System.out.println("Benchmark: " + state);
+
+            final int iterations = 30_000;
+
+            System.out.println("Preparing Give messages....");
+            final List<Give> gives = IntStream.range(0, iterations)
+                .mapToObj(i ->
+                    new Give()
+                        .address(clients.get(0).address)
+                        .timestampUS(UniqueMicroTimeProvider.INSTANCE.currentTimeMicros())
+                        .init(clients.get(1).address, 1)
+                )
+                .collect(toList());
+
+
+            final long start = System.nanoTime();
+            System.out.println("Running benchmark...");
+            for (int i = 0; i < iterations; i++) {
+                clients.get(0).toDefault().give(gives.get(i));
+            }
+            final long duration = System.nanoTime() - start;
+            final double ratio = ((double) iterations) * 1e9 / (double) duration;
+            System.out.format("Completed %,d iterations in %,d ms (%,.0f TPS)%n", iterations, duration / 1000000, ratio);
+
+            Jvm.pause(500);
+        }
+
         clients.forEach(client -> {
-            System.out.println("Waiting");
-            Jvm.pause(2000);
             System.out.println("Closing " + client.name());
             client.close();
 
         });
+
         System.out.println("Done.");
-        Jvm.pause(7000);
 
     }
 
